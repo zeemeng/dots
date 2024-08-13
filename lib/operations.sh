78cbfac600cda3c3ccc0f2c10cced25bb8702e7b
Zi Han Meng <zi.han.meng100@gmail.com>
2024-08-13 14:41:33 -0400

    Added operation feedback about skipping packages. Refactored package selection logic.

--- lib/operations.sh
+++ lib/operations.sh
@@ -17,53 +17,59 @@ seek_optional_confirmation() {
 	fi
 }
 
-append_to_selected_pkgs() {
-	# Expecting pre-defined variables: PKG_REPO, PKG, SELECTED_PKGS, NEW_LINE
+# Depends on pre-defined variables: PKG_REPO, PKG, REQUESTED_PKGS, INSTALL_PKGS, SETUP_PKGS, SKIP_PKGS, NEW_LINE
+append_to_pkg_lists() {
+	# Check if PKG contains any non-whitespace character. If it does, continue, otherwise return
+	if ! echo "$PKG" | grep -q "[^[:space:]]"; then return; fi
 
 	# Skip the meta package 'default' which holds the default PKG scripts
 	if [ "default" = "$PKG" ]; then return; fi
 
+	# Append to the list of requested pkgs
+	REQUESTED_PKGS="${REQUESTED_PKGS}${PKG}${NEW_LINE}"
+
 	# If any line in the "$PKG_REPO/$PKG/platform" file is a case insensitive BRE matching any part
 	# of the output of `uname -s`, continue processing, else return.
-	if ! { [ -f "$PKG_REPO/$PKG/platform" ] && uname -s | grep -i -q -f "$PKG_REPO/$PKG/platform"; }; then return; fi
+	if [ -f "$PKG_REPO/$PKG/platform" ] && ! { uname -s | grep -i -q -f "$PKG_REPO/$PKG/platform"; }; then
+		SKIP_PKGS="${SKIP_PKGS}${PKG}${NEW_LINE}"
+		return
+	fi
 
-	# Check if PKG contains any non-whitespace character. If it does, continue, otherwise return
-	echo "$PKG" | grep -q "[^[:space:]]" || return
-
-	# Add PKG to SELECTED_PKGS on a new line, suffixed with ":nis"
 	if [ -f "$PKG_REPO/$PKG/noinstall" ] && [ -f "$PKG_REPO/$PKG/nosetup" ]; then
-		SELECTED_PKGS="${SELECTED_PKGS}${NEW_LINE}${PKG}:nis"
+		SKIP_PKGS="${SKIP_PKGS}${PKG}${NEW_LINE}"
 		return
 	fi
 
-	# Add PKG to SELECTED_PKGS on a new line, suffixed with ":ni"
 	if [ -f "$PKG_REPO/$PKG/noinstall" ]; then
-		SELECTED_PKGS="${SELECTED_PKGS}${NEW_LINE}${PKG}:ni"
+		SETUP_PKGS="${SETUP_PKGS}${PKG}${NEW_LINE}"
 		return
 	fi
 
-	# Add PKG to SELECTED_PKGS on a new line, suffixed with ":ns"
 	if [ -f "$PKG_REPO/$PKG/nosetup" ]; then
-		SELECTED_PKGS="${SELECTED_PKGS}${NEW_LINE}${PKG}:ns"
+		INSTALL_PKGS="${INSTALL_PKGS}${PKG}${NEW_LINE}"
 		return
 	fi
 
-	# Add PKG to SELECTED_PKGS on a new line, suffixed with ":is"
 	if [ -d "$PKG_REPO/$PKG" ]; then
-		SELECTED_PKGS="${SELECTED_PKGS}${NEW_LINE}${PKG}:is"
+		SETUP_PKGS="${SETUP_PKGS}${PKG}${NEW_LINE}"
+		INSTALL_PKGS="${INSTALL_PKGS}${PKG}${NEW_LINE}"
 		return
 	fi
 }
 
 read_selected_packages() {
-	SELECTED_PKGS=""; NEW_LINE="
+	REQUESTED_PKGS=""
+	INSTALL_PKGS=""
+	SETUP_PKGS=""
+	SKIP_PKGS=""
+	NEW_LINE="
 "
 	# Select packages from operands
-	for PKG in "$@"; do append_to_selected_pkgs; done
+	for PKG in "$@"; do append_to_pkg_lists; done
 
 	# If specified file exists and can be read, select packages from file line-by-line
 	if [ -f "$f" ] && [ -r "$f" ]; then
-		while read PKG; do append_to_selected_pkgs; done < "$f"
+		while read PKG; do append_to_pkg_lists; done < "$f"
 
 	# Specified file exists, but cannot be read
 	elif [ "$f" ]; then
@@ -75,26 +81,35 @@ read_selected_packages() {
 	# If no operand and no package-list file is specified, select all packages from the target package repository
 	if [ "$#" -eq 0 ] && [ ! "$f" ]; then
 		while read PKG; do
-			append_to_selected_pkgs
+			append_to_pkg_lists
 		done <<-EOF
 		$(ls -1A "$PKG_REPO")
 		EOF
 	fi
 
-	# Sort and remove duplicate items in SELECTED_PKGS
-	SELECTED_PKGS="$(echo "$SELECTED_PKGS" | sort -u)"
+	# Sort and remove duplicate items in PKG lists
+	REQUESTED_PKGS="$(printf "$REQUESTED_PKGS" | sort -u)"
+	INSTALL_PKGS="$(printf "$INSTALL_PKGS" | sort -u)"
+	SETUP_PKGS="$(printf "$SETUP_PKGS" | sort -u)"
+	SKIP_PKGS="$(printf "$SKIP_PKGS" | sort -u)"
 }
 
 print_exit_selected_packages() {
-	NO_SUFFIX="$(echo "$SELECTED_PKGS" | sed -n 's/:..$//p; s/:nis$//p')"
-
 	# Print header
-	printf "%-20s%-20s%-20s%-20s%-20s\n" "Package Name" "Install" "Uninstall" "Configure" "RM Config"
-	printf "=========================================================================================\n"
+	printf "%-20s%-20s%-20s%-20s%-20s%-20s\n" "Package Name" "Platform" "Install" "Uninstall" "Setup" "Unset"
+	printf "=============================================================================================================\n"
 
-	printf "$NO_SUFFIX\n" | while read PKG; do
+	# DEBUG
+	# printf "===$REQUESTED_PKGS===\n"
+	echo "$REQUESTED_PKGS" | while read PKG; do
 		# "Package Name" column
 		printf "%-20s" "$PKG"
+
+		PLATFORM_COLUMN='ALL'
+		if [ -f "$PKG_REPO/$PKG/platform" ]; then
+			PLATFORM_COLUMN="$(tr '\n' ',' < "$PKG_REPO/$PKG/platform" | sed 's/,$//')"
+		fi
+		printf "%-20s" "$PLATFORM_COLUMN"
 
 		# "Install" and "Uninstall" columns
 		INSTALL_COLUMN=""; UNINSTALL_COLUMN=""; # Reset vars for each iteration
@@ -121,9 +136,16 @@ print_exit_selected_packages() {
 			if [ -f "$PKG_REPO/$PKG/unset" ]; then RM_CONFIG_COLUMN="CUSTOM"; else RM_CONFIG_COLUMN="DEFAULT"; fi
 		fi
 		printf "%-20s%-20s" "$CONFIG_COLUMN" "$RM_CONFIG_COLUMN"
-
 		printf "\n"
 	done
+	exit 0
+}
+
+warn_about_skipped_packages() {
+	if [ -z "$SKIP_PKGS" ]; then return; fi
+
+	log_warning "Skipping the following package(s) as they do not support the requested operation(s) on the current platform:"
+	printf "$SKIP_PKGS\n\n"
 }
 
 execute_script() (
@@ -157,17 +179,12 @@ dispatch_operations() {
 	OPERATION="$1" # Values: 'install' | 'setup' | 'uninstall' | 'unset'
 
 	# Update/sync back-end package manager repositories. Set options and environment variables
-	if [ "$OPERATION" = 'install' ] || [ "$OPERATION" = 'uninstall' ]; then init_pkg_manager; fi
-
-	# Find packages suitable for the target operation among SELECTED_PKGS
 	if [ "$OPERATION" = 'install' ] || [ "$OPERATION" = 'uninstall' ]; then
-		SED_EXPR='s/:is$//p; s/:ns$//p'
+		init_pkg_manager;
+		TARGET_PKGS="$INSTALL_PKGS"
 	else # For OPERATION == 'setup' | 'unset'
-		SED_EXPR='s/:is$//p; s/:ni$//p'
+		TARGET_PKGS="$SETUP_PKGS"
 	fi
-
-	TARGET_PKGS="$(echo "$SELECTED_PKGS" | sed -n "$SED_EXPR")"
-	if [ -z "$TARGET_PKGS" ]; then return; fi
 
 	log_info "Packages selected for $(highlight_string "$OPERATION"):\n$TARGET_PKGS\n"
 	seek_optional_confirmation
